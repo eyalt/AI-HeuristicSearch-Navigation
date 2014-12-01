fb3d737384a89331fa6b9a8fd5a1301541c93f86
eyalt <eyaltolchinsky@gmail.com>
2014-12-02 01:56:43 +0200

    undone a_star

--- search.py
+++ search.py
@@ -1,8 +1,17 @@
 from __future__ import division, print_function
 from ways import load_map_from_csv
 'implement your search methods here'
+from collections import namedtuple
  
-
+Problem = namedtuple('Problem',['init_state','goal','expand','id']) 
+Node = namedtuple('node', ['state','parent','h','g','f'])
+ 
+def run_astar(problem,h_func,g_func):
+    hi = h_func(problem.init_state)
+    open = [Node(problem.init_state,None,hi,0,hi)]
+    while open:
+        next = open.pop(0)
+    
 
 
 
