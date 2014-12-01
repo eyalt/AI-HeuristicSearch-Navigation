111ecb2a308018b1ac724e873312a0840cb4663f
eyalt <eyaltolchinsky@gmail.com>
2014-12-02 01:13:48 +0200

    Hakuna matata. stats.py workaka

--- stats.py
+++ stats.py
@@ -4,7 +4,8 @@ $ python stats.py
 '''
 from __future__ import division, print_function
 from collections import namedtuple
-from ways import load_map_from_csv
+from ways import load_map_from_csv, info
+import collections
 
 
 # Q7:
@@ -12,14 +13,24 @@ def map_statistics(roads):
     '''return a dictionary containing the desired information
     You can edit this function as you wish'''
     Stat = namedtuple('Stat', ['max', 'min', 'avg'])
+    junctions = roads.junctions()
+    
+    linksNum = [len(j.links) for j in junctions]
+    
+    typeHist = collections.Counter()
+    for lnk in roads.iterlinks():
+        typeHist[info.ROAD_TYPES[lnk.highway_type]] += 1
+    
     return {
-        'Number of junctions' : None,
-        'Number of links' : None,
-        'Outgoing branching factor' : Stat(max=None, min=None, avg=None),
-        'Link distance' : Stat(max=None, min=None, avg=None),
+        'Number of junctions' : len(junctions),
+        'Number of links' : sum(linksNum),
+        'Outgoing branching factor' : Stat(max=max(linksNum),
+                                            min=min(linksNum), 
+                                            avg=sum(linksNum)/float(len(linksNum))),
+        'Link distance' : Stat(max=max(l.distance for l in roads.iterlinks()), min=min(l.distance for l in roads.iterlinks()), avg=sum(l.distance for l in roads.iterlinks())/float(len(list(roads.iterlinks())))),
         # value should be a dictionary
         # mapping each road_info.TYPE to the no' of links of this type
-        'Link type histogram' : None,  # tip: use collections.Counter
+        'Link type histogram' : dict(typeHist),  # tip: use collections.Counter
     }
 
 
