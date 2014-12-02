26b84c761d4586e5f812b6b8b708d90148e4db8e
unknown <itaykhazon@gmail.com>
2014-12-02 02:35:05 +0200

    implemented astar and get_path now just need to implement the classes close_set, open_set

--- search.py
+++ search.py
@@ -3,16 +3,90 @@ from ways import load_map_from_csv
 'implement your search methods here'
 from collections import namedtuple
  
-Problem = namedtuple('Problem',['init_state','goal','expand','id']) 
+Problem = namedtuple('Problem',['init_state','goal_func','expand','id']) 
 Node = namedtuple('node', ['state','parent','h','g','f'])
- 
-def run_astar(problem,h_func,g_func):
-    hi = h_func(problem.init_state)
-    open = [Node(problem.init_state,None,hi,0,hi)]
-    while open:
-        next = open.pop(0)
-    
 
+class open_set(object):
+    def __init__(self):
+        pass
+    
+    def add(self, state):
+        pass
+    
+    def remove(self, state):
+        pass
+    
+    def pop_best(self):
+        pass
+ 
+    def find_state(self):
+        pass
+
+    def is_empty(self):
+        pass
+ 
+class close_set(object):
+    def __init__(self):
+        pass
+    
+    def add(self, state):
+        pass
+    
+    def remove(self, state):
+        pass
+
+    def find_state(self, state):
+        pass
+
+def get_path(node):
+    '''
+    returns the path that ends in this node according to the parant pointers.
+    '''
+    path = [node]
+    while path[0].parent:
+        path = [path[0].parent]+path
+    return [x.state for x in path]
+ 
+def run_astar(problem,h_func,cost_func):
+    #initializing the algorithm groups
+    hi = h_func(problem.init_state)
+    closed = close_set()
+    opened = open_set()
+    opened.add(Node(problem.init_state,None,hi,0,hi))
+
+    while not opened.is_empty():
+        next_node = opened.pop_best()
+        closed.add(next_node)
+        if problem.goal_func(next_node):
+            return get_path(next_node)
+        for s in problem.expand(next_node.state):
+            new_g = next_node.g + cost_func(next_node.state,s)
+            old_node = opened.find_state(s)
+            if old_node: # a node with state s is in opened
+                if new_g < old_node.g:
+                    old_node.g = new_g
+                    old_node.parent = next_node
+                    old_node.f = old_node.g + old_node.h
+                    opened.remove(old_node)
+                    opened.add(old_node)
+                else: # old path is better - do nothing
+                    pass
+            else: # state isn't in opened - maybe in closed
+                old_node = closed.find_state(s)
+                if old_node:
+                    if new_g < old_node.g:
+                        old_node.g = new_g
+                        old_node.parent = next_node
+                        old_node.f = old_node.g + old_node.h
+                        closed.remove(old_node)
+                        closed.add(old_node)
+                    else: # old path is better - do nothing
+                        pass
+                else:
+                    state_h = h_func(s)
+                    new_node = Node(s,next_node,state_h,new_g,state_h+new_g)
+                    open.add(new_node)
+        return None
 
 
 if __name__ == '__main__':
